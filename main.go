2113cfeaf6f6656385361e348540f6ccef1550ba
Pierre Leroy <113354707+Jayksio@users.noreply.github.com>
2024-01-18 10:27:37 +0100

    install Go Fiber

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
