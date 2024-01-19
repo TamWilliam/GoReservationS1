ac09772a6a554fa7bee0e7699bf7a7b41266108c
Pierre Leroy <113354707+Jayksio@users.noreply.github.com>
2024-01-19 09:25:43 +0100

    template par défaut

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module projetgoreservation
 go 1.21.6
 
 require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/howeyc/fsnotify v0.9.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/pilu/config v0.0.0-20131214182432-3eb99e6c0b9a // indirect
--- main.go
+++ main.go
@@ -1,32 +1,88 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
-type Todo struct {
-	Title string
-	Done  bool
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "domapi92"
+	dbname   = "projetgoreservation"
+)
+
+type Hairdresser struct {
+	IDHairdresser int
+	FirstName     string
+	LastName      string
+	IDHairSalon   int
 }
 
-type TodoPageData struct {
-	PageTitle string
-	Todos     []Todo
+type HairdresserPageData struct {
+	PageTitle    string
+	Hairdressers []Hairdresser
 }
 
 func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
 	tmpl := template.Must(template.ParseFiles("templates/salon-de-coiffure.html"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data := TodoPageData{
-			PageTitle: "My TODO list",
-			Todos: []Todo{
-				{Title: "Task 1", Done: false},
-				{Title: "Task 2", Done: true},
-				{Title: "Task 3", Done: true},
-			},
+		var hairdressers []Hairdresser
+
+		if db != nil {
+			rows, err := db.Query("SELECT * FROM hairdresser")
+			if err != nil {
+				log.Printf("Erreur lors de l'exécution de la requête: %v", err)
+				http.Error(w, "Erreur interne du serveur", http.StatusInternalServerError)
+				return
+			}
+			defer rows.Close()
+
+			for rows.Next() {
+				var h Hairdresser
+				if err := rows.Scan(&h.IDHairdresser, &h.FirstName, &h.LastName, &h.IDHairSalon); err != nil {
+					log.Printf("Erreur lors de la lecture des lignes: %v", err)
+					continue
+				}
+				hairdressers = append(hairdressers, h)
+			}
+
+			if err := rows.Err(); err != nil {
+				log.Printf("Erreur lors de la récupération des lignes: %v", err)
+			}
+		}
+
+		data := HairdresserPageData{
+			PageTitle:    "Liste des coiffeurs",
+			Hairdressers: hairdressers,
 		}
 		tmpl.Execute(w, data)
 	})
-	http.ListenAndServe(":80", nil)
+
+	log.Println("Le serveur démarre sur le port :80")
+	err = http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Fatal("Erreur lors du démarrage du serveur: ", err)
+	}
 }
