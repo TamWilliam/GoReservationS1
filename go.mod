8480b74dbbe46a5b8d0f06e92a55daea75d5a3c1
TamWilliam <72071669+TamWilliam@users.noreply.github.com>
2024-02-03 16:07:37 +0100

    page reservation (non fonctionnel)

--- go.mod
+++ go.mod
@@ -1,3 +1,27 @@
 module projetgoreservation
 
 go 1.21.6
+
+require gorm.io/driver/postgres v1.5.4
+
+require github.com/joho/godotenv v1.5.1 // indirect
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/howeyc/fsnotify v0.9.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/lib/pq v1.10.9
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/pilu/config v0.0.0-20131214182432-3eb99e6c0b9a // indirect
+	github.com/pilu/fresh v0.0.0-20190826141211-0fa698148017 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	gorm.io/driver/mysql v1.5.2 // indirect
+	gorm.io/gorm v1.25.6 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,147 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+// Data structures
+type Customer struct {
+	IDCustomer int
+	Email      string
+}
+
+type HairSalon struct {
+	IDHairSalon int
+	Name        string
+}
+
+type HairDresser struct {
+	IDHairDresser int
+	FirstName     string
+}
+
+type PageVariables struct {
+	PageTitle    string
+	Utilisateurs []Customer
+	Salons       []HairSalon
+	Coiffeurs    []HairDresser
+}
+
+var tmpl *template.Template
+
+func init() {
+	tmpl = template.Must(template.ParseFiles("templates/reservation.html"))
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	db, err := sql.Open("postgres", fmt.Sprintf("user=%s dbname=%s password=%s sslmode=disable",
+		os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PASSWORD")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	http.HandleFunc("/reservation", func(w http.ResponseWriter, r *http.Request) {
+		utilisateurs, err := getCustomers(db)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		salons, err := getHairSalons(db)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		coiffeurs, err := getHairDressers(db)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		pageVariables := PageVariables{
+			PageTitle:    "Réservation",
+			Utilisateurs: utilisateurs,
+			Salons:       salons,
+			Coiffeurs:    coiffeurs,
+		}
+
+		tmpl.Execute(w, pageVariables)
+	})
+
+	log.Fatal(http.ListenAndServe(":6060", nil))
+}
+
+func getCustomers(db *sql.DB) ([]Customer, error) {
+	rows, err := db.Query("SELECT id_customer, email FROM customers")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var customers []Customer
+	for rows.Next() {
+		var customer Customer
+		err := rows.Scan(&customer.IDCustomer, &customer.Email)
+		if err != nil {
+			return nil, err
+		}
+		customers = append(customers, customer)
+	}
+
+	return customers, nil
+}
+
+func getHairSalons(db *sql.DB) ([]HairSalon, error) {
+	rows, err := db.Query("SELECT id_hair_salon, name FROM hair_salons")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var salons []HairSalon
+	for rows.Next() {
+		var salon HairSalon
+		err := rows.Scan(&salon.IDHairSalon, &salon.Name)
+		if err != nil {
+			return nil, err
+		}
+		salons = append(salons, salon)
+	}
+
+	return salons, nil
+}
+
+func getHairDressers(db *sql.DB) ([]HairDresser, error) {
+	rows, err := db.Query("SELECT id_hair_dresser, first_name FROM hair_dressers")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var coiffeurs []HairDresser
+	for rows.Next() {
+		var coiffeur HairDresser
+		err := rows.Scan(&coiffeur.IDHairDresser, &coiffeur.FirstName)
+		if err != nil {
+			return nil, err
+		}
+		coiffeurs = append(coiffeurs, coiffeur)
+	}
+
+	return coiffeurs, nil
 }
