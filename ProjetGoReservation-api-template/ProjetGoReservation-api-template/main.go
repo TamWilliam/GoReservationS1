5c5ffd5411d29ec105019b81df1f71ddf1cad81b
Pierre Leroy <113354707+Jayksio@users.noreply.github.com>
2024-02-06 20:06:41 +0100

    correction attribut name + ajout du password dans le type struct

--- ProjetGoReservation-api-template/ProjetGoReservation-api-template/main.go
+++ ProjetGoReservation-api-template/ProjetGoReservation-api-template/main.go
@@ -15,6 +15,7 @@ type Customer struct {
 	FirstName  string `json:"first_name"`
 	LastName   string `json:"last_name"`
 	Email      string `json:"email"`
+	Password   string `json:"password"`
 }
 
 func main() {
